c51a52fa8ed5f9201257227e3b057b3a72fcd58d
Gerald Lang <>
2019-12-03 17:58:31 -0700

    Flask templating

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, Flask!"
+	user = {'username': 'Gerald'}
+	posts = [
+		{
+			'author': {'username': 'Micky'},
+			'body': 'Herka derka mcderka'
+		},
+		{
+			'author': {'username': 'Annie'},
+			'body': 'Meowy meow meow meow'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
